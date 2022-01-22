34a1ad6861bd37645d49abb3a999b8b4fe3611c1
bkacmaz <berfinnkacmaz@gmail.com>
2022-01-22 16:32:36 +0300

    first commit

---
+++ Recursive/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Recursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Re(0);
+            Console.WriteLine("Hello World!");
+
+            int Re (int i)
+            {
+                i++;
+                if (i < 5)
+                {
+                    Re(i);
+                }
+                return i;
+            }
+
+        }
+    }
+}
